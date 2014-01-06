5b3b48d65c98c62dedcc55196ac1e598cf2788dd
saerem21 <nemessisdds>
2014-01-06 17:50:10 +0900

    테이블뷰 편집 모드입니다..
    182page

--- Ex182_ex13/ViewController.m
+++ Ex182_ex13/ViewController.m
@@ -6,18 +6,56 @@
 //  Copyright (c) 2014년 SDT-1. All rights reserved.
 //
 
-#import "ViewController.h"
 
-@interface ViewController ()
+#import "ViewController.h"
+#define CELL_ID @"CELL_ID"
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
 @implementation ViewController
+- (IBAction)toggleEdit:(id)sender {
+    self.table.editing = !self.table.editing;
+}
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.row % 3 == 0){
+        return UITableViewCellEditingStyleNone;
+    }
+    else if(indexPath.row % 3 == 1){
+        return UITableViewCellEditingStyleDelete;
+    }
+    else{
+        return UITableViewCellEditingStyleInsert;
+    }
+
+}
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(UITableViewCellEditingStyleDelete == editingStyle){
+        NSLog(@"del %d",indexPath.row);
+    }
+    else{
+        NSLog(@"input %d",indexPath.row);
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL_ID"];
+    cell.textLabel.text = [NSString stringWithFormat:@"cell %d",indexPath.row];
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //data =[[NSMutableArray alloc]initWithObjects:@"a",@"b",@"c",@"d",@"e",@"f", nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +65,4 @@
 }
 
 @end
+
